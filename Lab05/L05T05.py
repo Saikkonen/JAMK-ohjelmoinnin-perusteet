176dec0e6bccbe3e0ef2a36fed7e1d42c8f070df
Aatu Saikkonen <AC2136@student.jamk.fi>
2021-11-15 10:38:34 +0200

    Lab05

--- Lab05/L05T05.py
+++ Lab05/L05T05.py
@@ -1,6 +1,7 @@
 def get_cost(kilometrit,kulutus,hinta):
-    print(round(((kilometrit*kulutus)/100)*hinta, 2)), "€"
-
+    vastaus = round(((kilometrit*kulutus)/100)*hinta, 2)
+    x = format(vastaus, ".2f")
+    print(x+"€")
 
 get_cost(100,7.5,1.88)
 get_cost(220,6.9,1.88)
