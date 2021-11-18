47fe61f4956fca4977f5e1b47a78e383cd33c731
Aatu Saikkonen <AC2136@student.jamk.fi>
2021-11-18 11:09:19 +0200

    test

---
+++ Lab07/L07T01.py
@@ -0,0 +1,8 @@
+class pelikortti(object):
+
+    def __init__(self, maa, arvo):
+        self.maa = maa
+        self.arvo = arvo
+    
+    def __init__(self):
+        return "Maa: " + self.maa + ", " + str(self.arvo)
